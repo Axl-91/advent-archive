f9988780122ad44683d7a35c458fbaf4e7b855c8
Axel Aparicio Rotermund <axelaparicior@gmail.com>
2025-12-21 22:38:40 -0300

    Finished day2/part I on Advent24

--- advent24/day2/main.ts
+++ advent24/day2/main.ts
@@ -5,14 +5,33 @@ import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+function isDiffValid(max: number, min: number) {
+  return max - min >= 1 && max - min <= 3;
+}
+
 function main() {
-  let filePath = path.join(__dirname, "input_test");
+  let result = 0;
+  let filePath = path.join(__dirname, "input");
   let inputStr = readFileSync(filePath, "utf-8");
-  const lines = inputStr.split("\n");
+  const lines = inputStr.trim().split("\n");
 
   for (const line of lines) {
-    console.log(line);
+    let numbers = line.split(" ").map(Number);
+
+    let isAsc = numbers.every(
+      (num, i, arr) =>
+        i === 0 || (num > arr[i - 1]! && isDiffValid(num, arr[i - 1]!)),
+    );
+
+    let isDesc = numbers.every(
+      (num, i, arr) =>
+        i === 0 || (num < arr[i - 1]! && isDiffValid(arr[i - 1]!, num)),
+    );
+
+    if (isAsc || isDesc) result++;
   }
+  console.log(result);
+
   return 0;
 }
 
