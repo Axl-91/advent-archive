d3a768d91847d70fe888f040d5ca35443416d603
Axel Aparicio Rotermund <axelaparicior@gmail.com>
2025-12-22 21:26:43 -0300

    Finished day2/part II on Advent24

--- advent24/day2/main.ts
+++ advent24/day2/main.ts
@@ -30,15 +30,32 @@ function part1(inputLines: string[]) {
   console.log(`Part 1 Result: ${result}`);
 }
 
-// function part2(inputLines: string[]) {
-//   let result = 0;
+function part2(inputLines: string[]) {
+  let result = 0;
 
-//   for (const line of inputLines) {
-//     console.log(line);
-//   }
+  for (const line of inputLines) {
+    let numbers = line.split(" ").map(Number);
 
-//   console.log(`Part 2 Result: ${result}`);
-// }
+    for (let i = 0; i < numbers.length; i++) {
+      let new_numbers = numbers.filter((_, idx) => i != idx);
+
+      let isAsc = new_numbers.every(
+        (num, i, arr) =>
+          i === 0 || (num > arr[i - 1]! && isDiffValid(num, arr[i - 1]!)),
+      );
+      let isDesc = new_numbers.every(
+        (num, i, arr) =>
+          i === 0 || (num < arr[i - 1]! && isDiffValid(arr[i - 1]!, num)),
+      );
+
+      if (isAsc || isDesc) {
+        result++;
+        break;
+      }
+    }
+  }
+  console.log(`Part 2 Result: ${result}`);
+}
 
 function main() {
   let filePath = path.join(__dirname, "input");
@@ -46,6 +63,7 @@ function main() {
   const inputLines = inputStr.trim().split("\n");
 
   part1(inputLines);
+  part2(inputLines);
 
   return 0;
 }
